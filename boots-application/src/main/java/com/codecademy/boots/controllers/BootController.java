9bf8a581efd1d6837c0ccdfa959e0df7da741397
Khayyam Saleem <hello@khayyam.me>
2021-06-30 12:29:01 -0400

    CDEV-978: adds search method to solution code

--- boots-application/src/main/java/com/codecademy/boots/controllers/BootController.java
+++ boots-application/src/main/java/com/codecademy/boots/controllers/BootController.java
@@ -3,6 +3,7 @@ package com.codecademy.boots.controllers;
 import java.lang.Iterable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Arrays;
 import java.util.Optional;
 
@@ -18,27 +19,81 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.codecademy.boots.entities.Boot;
 import com.codecademy.boots.enums.BootType;
+import com.codecademy.boots.exceptions.QueryNotSupportedException;
 import com.codecademy.boots.repositories.BootRepository;
 
 @RestController
 @RequestMapping("/api/v1/boots")
 public class BootController {
 
-    private final BootRepository bootRepository;
+	private final BootRepository bootRepository;
 
-    public BootController(final BootRepository bootRepository) {
-        this.bootRepository = bootRepository;
-    }
+	public BootController(final BootRepository bootRepository) {
+		this.bootRepository = bootRepository;
+	}
 
-    @GetMapping("/")
-    public Iterable<Boot> getAllBoots() {
-        return this.bootRepository.findAll();
-    }
+	@GetMapping("/")
+	public Iterable<Boot> getAllBoots() {
+		return this.bootRepository.findAll();
+	}
 
-    @GetMapping("/types")
-    public List<BootType> getBootTypes() {
-        return Arrays.asList(BootType.values());
-    }
+	@GetMapping("/types")
+	public List<BootType> getBootTypes() {
+		return Arrays.asList(BootType.values());
+	}
 
-
+	@GetMapping("/search")
+	public List<Boot> searchBoots(
+		@RequestParam(required = false) String material,
+		@RequestParam(required = false) BootType type,
+		@RequestParam(required = false) Float size,
+		@RequestParam(required = false) Integer minQuantity
+	) throws QueryNotSupportedException {
+		if (Objects.nonNull(material)) {
+			if (Objects.nonNull(type) && Objects.nonNull(size) && Objects.nonNull(minQuantity)) {
+				// call the repository method that accepts a material, type, size, and minimum quantity
+				return this.bootRepository.findByMaterialAndSizeAndTypeAndQuantityGreaterThan(material, size, type, minQuantity);
+			} else if (Objects.nonNull(type) && Objects.nonNull(size)) {
+				// call the repository method that accepts a material, size, and type
+				return this.bootRepository.findByMaterialAndSizeAndType(material, size, type);
+			} else if (Objects.nonNull(type) && Objects.nonNull(minQuantity)) {
+				// call the repository method that accepts a material, a type, and a minimum quantity
+				return this.bootRepository.findByMaterialAndTypeAndQuantityGreaterThan(material, type, minQuantity);
+			} else if (Objects.nonNull(type)) {
+				// call the repository method that accepts a material and a type
+				return this.bootRepository.findByMaterialAndType(material, type);
+			} else {
+				// call the repository method that accepts only a material
+				return this.bootRepository.findByMaterial(material);
+			}
+		} else if (Objects.nonNull(type)) {
+			if (Objects.nonNull(size) && Objects.nonNull(minQuantity)) {
+				// call the repository method that accepts a type, size, and minimum quantity
+				return this.bootRepository.findByTypeAndSizeAndQuantityGreaterThan(type, size, minQuantity);
+			} else if (Objects.nonNull(size)) {
+				// call the repository method that accepts a type and a size
+				return this.bootRepository.findByTypeAndSize(type, size);
+			} else if (Objects.nonNull(minQuantity)) {
+				// call the repository method that accepts a type and a minimum quantity
+				return this.bootRepository.findByTypeAndQuantityGreaterThan(type, minQuantity);
+			} else {
+				// call the repository method that accept only a type
+				return this.bootRepository.findByType(type);
+			}
+		} else if (Objects.nonNull(size)) { 
+			if (Objects.nonNull(minQuantity)) {
+				// call the repository method that accepts a size and a minimum quantity
+				return this.bootRepository.findBySizeAndQuantityGreaterThan(size, minQuantity);
+			} else {
+				// call the repository method that accepts only a size
+				return this.bootRepository.findBySize(size);
+			}
+		} else if (Objects.nonNull(minQuantity)){
+			// call the repository method that accepts only a minimum quantity
+			return this.bootRepository.findByQuantityGreaterThan(minQuantity);
+		} else {
+			throw new QueryNotSupportedException("This query is not supported! Try a different combination of search parameters.");
+		}
+	}
+	
 }
