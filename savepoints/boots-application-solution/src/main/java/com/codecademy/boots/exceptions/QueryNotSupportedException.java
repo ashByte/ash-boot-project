1facc0fde121acf94132ac0b9ab71bd7e62fbe8c
Ashley Kim <gk1301@nyu.edu>
2023-08-10 18:13:54 -0500

    goodbye solution

--- savepoints/boots-application-solution/src/main/java/com/codecademy/boots/exceptions/QueryNotSupportedException.java
+++
@@ -1,7 +0,0 @@
-package com.codecademy.boots.exceptions;
-
-public class QueryNotSupportedException extends Exception {
-  public QueryNotSupportedException(String message) {
-    super(message);
-  }
-}
