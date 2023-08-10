38fd1a181f75aa62393d370a219693e7c786f2b6
Ashley Kim <gk1301@nyu.edu>
2023-08-10 18:16:40 -0500

    get rid of mystery pom file

--- savepoints/boots-application-starting/src/main/java/com/codecademy/boots/exceptions/NotImplementedException.java
+++
@@ -1,7 +0,0 @@
-package com.codecademy.boots.exceptions;
-
-public class NotImplementedException extends RuntimeException {
-  public NotImplementedException(String message) {
-    super(message);
-  }
-}
